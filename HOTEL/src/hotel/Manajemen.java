6a0e084bc1275a5476ead0225d4674b2ebef5e30
Cut Sula Fhatia Rahma <132277905+csulafr@users.noreply.github.com>
2023-12-14 13:37:59 +0700

    Update Manajemen.java

--- HOTEL/src/hotel/Manajemen.java
+++ HOTEL/src/hotel/Manajemen.java
@@ -38,3 +38,25 @@ public class Manajemen extends javax.swing.JFrame {
             return null;
         }
     }
+
+public void ViewData() {
+        try {
+            ResultSet resultSet = cekKamar();
+            DefaultTableModel model = (DefaultTableModel) tKosong.getModel(); // Ambil model yang sudah ada
+
+            // Kosongkan model yang ada sebelumnya
+            model.setRowCount(0);
+
+            while (resultSet.next()) {
+                model.addRow(new Object[]{
+                    resultSet.getString("nama"),
+                    resultSet.getString("nik"),
+                    resultSet.getString("no_kamar"),
+                    resultSet.getString("tanggal"),
+                    resultSet.getString("harga")
+                });
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
