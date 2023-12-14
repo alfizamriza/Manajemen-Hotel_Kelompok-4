5071d50f7f124bceddc21e096ec593a33ac1f6e2
IndrianiMizaAlfiyanti <132206065+IndrianiMizaAlfiyanti@users.noreply.github.com>
2023-12-14 15:06:25 +0700

    Update Login.java

--- HOTEL/src/hotel/Login.java
+++ HOTEL/src/hotel/Login.java
@@ -270,3 +270,12 @@ public class Login extends javax.swing.JFrame {
                     break;
                 }
             }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
