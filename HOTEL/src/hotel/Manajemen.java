8950b57ee16a1ae1ee54bd1419b90684d9526fb5
Cut Sula Fhatia Rahma <132277905+csulafr@users.noreply.github.com>
2023-12-14 13:51:50 +0700

    Update Manajemen.java

--- HOTEL/src/hotel/Manajemen.java
+++ HOTEL/src/hotel/Manajemen.java
@@ -330,3 +330,89 @@ public void Total() {
             }
         }
     }
+
+private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    Home HM = new Home();
+    HM.setVisible(true);
+    this.dispose();
+}//GEN-LAST:event_jButton1ActionPerformed
+
+private void bCariActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCariActionPerformed
+    pendapatan();
+    Total();
+    ViewData();
+}//GEN-LAST:event_bCariActionPerformed
+
+private void txPendapatanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txPendapatanActionPerformed
+    // TODO add your handling code here:
+}//GEN-LAST:event_txPendapatanActionPerformed
+
+private void jLabel8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseClicked
+    dispose();
+}//GEN-LAST:event_jLabel8MouseClicked
+int xx, xy;
+private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
+    xx = evt.getX();
+    xy = evt.getY();
+}//GEN-LAST:event_formMousePressed
+
+private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+    int x = evt.getXOnScreen();
+    int y = evt.getYOnScreen();
+    this.setLocation(x - xx, y - xy);
+}//GEN-LAST:event_formMouseDragged
+
+/**
+ * @param args the command line arguments
+ */
+public static void main(String args[]) {
+    /* Set the Nimbus look and feel */
+    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+     */
+    try {
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+    } catch (ClassNotFoundException ex) {
+        java.util.logging.Logger.getLogger(Manajemen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (InstantiationException ex) {
+        java.util.logging.Logger.getLogger(Manajemen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (IllegalAccessException ex) {
+        java.util.logging.Logger.getLogger(Manajemen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        java.util.logging.Logger.getLogger(Manajemen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    //</editor-fold>
+    //</editor-fold>
+    //</editor-fold>
+    //</editor-fold>
+
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new Manajemen().setVisible(true);
+        }
+    });
+}
+
+
+// Variables declaration - do not modify//GEN-BEGIN:variables
+private javax.swing.JButton bCari;
+private javax.swing.JButton jButton1;
+private javax.swing.JLabel jLabel1;
+private javax.swing.JLabel jLabel2;
+private javax.swing.JLabel jLabel3;
+private javax.swing.JLabel jLabel8;
+private javax.swing.JPanel jPanel1;
+private javax.swing.JPanel jPanel2;
+private javax.swing.JScrollPane jScrollPane1;
+private javax.swing.JTable tKosong;
+private javax.swing.JTextField txPendapatan;
+private javax.swing.JTextField txPengunjung;
+// End of variables declaration//GEN-END:variables
+}
