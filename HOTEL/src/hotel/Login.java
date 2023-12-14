40f7ef0ee3f66ba6b646a88c93806f0ff4daa41e
IndrianiMizaAlfiyanti <132206065+IndrianiMizaAlfiyanti@users.noreply.github.com>
2023-12-14 15:05:09 +0700

    Update Login.java

--- HOTEL/src/hotel/Login.java
+++ HOTEL/src/hotel/Login.java
@@ -253,3 +253,13 @@ public class Login extends javax.swing.JFrame {
     private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
         dispose();
     }//GEN-LAST:event_jLabel3MouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
