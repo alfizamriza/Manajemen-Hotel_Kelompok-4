1dc52aa64730bbc4cc2fc732830f566cd4607b57
IndrianiMizaAlfiyanti <132206065+IndrianiMizaAlfiyanti@users.noreply.github.com>
2023-12-14 15:07:03 +0700

    Update Login.java

--- HOTEL/src/hotel/Login.java
+++ HOTEL/src/hotel/Login.java
@@ -279,3 +279,12 @@ public class Login extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
