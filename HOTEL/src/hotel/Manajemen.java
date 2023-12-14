5980ab5be350934e1f7ba205e88885807fa94f81
Cut Sula Fhatia Rahma <132277905+csulafr@users.noreply.github.com>
2023-12-14 13:47:48 +0700

    Update Manajemen.java

--- HOTEL/src/hotel/Manajemen.java
+++ HOTEL/src/hotel/Manajemen.java
@@ -257,3 +257,6 @@ public void ViewData() {
         );
 
     getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+    pack();
+}// </editor-fold>//GEN-END:initComponents
