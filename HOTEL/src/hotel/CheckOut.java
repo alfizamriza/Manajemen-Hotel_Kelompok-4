6c87383f688bd2e697d2957461c3d609fd47aa10
nurimasyithah <131990558+nurimasyithah@users.noreply.github.com>
2023-12-14 14:52:21 +0700

    Update CheckOut.java

--- HOTEL/src/hotel/CheckOut.java
+++ HOTEL/src/hotel/CheckOut.java
@@ -172,3 +172,12 @@ public class CheckOut extends javax.swing.JFrame {
                 CariActionPerformed(evt);
             }
         });
+
+        bCari.setBackground(new java.awt.Color(51, 51, 255));
+        bCari.setForeground(new java.awt.Color(255, 255, 255));
+        bCari.setText("Cari");
+        bCari.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bCariActionPerformed(evt);
+            }
+        });
